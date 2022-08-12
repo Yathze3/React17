2fd835f43bce164ca847961aca78a57cd02f82b1
Adrian Andal <adrian.andal@msc.com>
2022-08-12 15:47:24 +0200

    Configuring Your own environment

---
+++ fulljs/webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+          },
+        },
+      ],
+    },
+    plugins: [
+        new webpack.ProvidePlugin({
+           "React": "react",
+        }),
+     ],
+     externals: {
+        'react': 'React'
+    },
+  };
